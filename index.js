705d326b5d5b7dfeef2645c940aeb5c4ece6d943
Vlad Step <sobol_v92@mail.ru>
2024-04-01 21:21:06 +0300

    add class work 2

--- index.js
+++ index.js
@@ -7,6 +7,9 @@
 
 
 const clearBtn = document.getElementById('clearC')
+const screen = window.document.getElementsByClassName('calc__screen temp')
+const calc = document.querySelector('.calc__btn-container')
+console.dir(calc);
 
 
 const printLog = function() {
@@ -41,8 +44,24 @@ clearBtn.addEventListener('click', function(event) {
 const digits = document.querySelectorAll('.digit')
 console.log(digits);
 
-for (let i = 0; i < digits.length; i++) {
-    digits[i].addEventListener('click', function(event) {
-        console.log(event.target.textContent);
-    })
-}
+// for (let i = 0; i < digits.length; i++) {
+//     digits[i].addEventListener('click', function(event) {
+//         screen[0].value = event.target.textContent
+//     })
+// }
+
+calc.addEventListener('click', function(event) {
+    console.dir(event.target);
+    if (event.target.classList.contains('digit')) {
+        screen[0].value = event.target.textContent
+    }
+})
+
+// получение элментов 
+// не забывать что некоторые способы возвращают коллекцию
+// что такое событие?
+// 3 способа навесить слушатель события на элемент
+// знать addEventListener, какие аргументы нужно передавать
+// что приходит в качестве параметра в передаваемый второй аргумент 
+// что такое target
+// classList, attributes, classList.contains
